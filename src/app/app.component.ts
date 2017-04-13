dbfb23771856093c3c0b01aeeee9ead5b3dd84b3
lichangxiao <lichangxiao@j1.com>
2017-04-13 15:01:42 +0800

    angular2

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  // 或者使用以下用法
+  // template: '<p>{{title}}</p>'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app angular2!';
+}
